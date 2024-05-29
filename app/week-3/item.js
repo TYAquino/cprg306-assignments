8d7e8c3ec9b5da0d88241334103e48a21eacbb8d
Nabby <trishasalubreaquino@gmail.com>
2024-05-28 23:13:43 -0600

    coms

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function Item({ name, quantity, category }) {
   return (
-    <li class="border border-sky-500 w-full max-w-xs m-4 p-2 flex items-center list-inside list-disc">
-      <div class="w-full">
-        <p class="text-black-600 font-semibold">{name}</p>
-        <p class="text-sm text-black-600">Quantity: {quantity}</p>
-        <p class="text-sm text-black-600">Category: {category}</p>
+    <li class="border border-sky-500 mx-auto w-full max-w-xs m-4 p-2 list-none">
+      <div>
+        <p class="text-black-600 font-semibold text-center">{name}</p>
+        <p class="text-sm text-black-600 text-center">Quantity: {quantity}</p>
+        <p class="text-sm text-black-600 text-center">Category: {category}</p>
       </div>
     </li>
   );

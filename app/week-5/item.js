0add6b47b5368eb1c0a4e76d995cfea0d1d287de
Nabby <trishasalubreaquino@gmail.com>
2024-06-11 18:11:30 -0600

    commit

--- app/week-5/item.js
+++ app/week-5/item.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Item({ name, quantity, category }) {
   return (
-    <li class="border rounded border-sky-500 mx-auto w-full max-w-xs m-4 p-2 list-none hover:scale-110 transition duration-300 ease-in-out hover:bg-gray-800">
+    <li class="border rounded border-sky-500 mx-auto w-full max-w-xs m-4 p-2 list-none hover:scale-110 transition duration-300 ease-in-out">
       <div>
         <p class="text-black-600 font-semibold text-center">{name}</p>
         <p class="text-sm text-black-600 text-center">Quantity: {quantity}</p>

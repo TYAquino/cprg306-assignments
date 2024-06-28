bd6de087165d30e49038e86d6bbb73f10caf15b5
Nabby <trishasalubreaquino@gmail.com>
2024-06-28 15:54:08 -0600

    commit

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -18,27 +18,6 @@ export default function ItemList({ items }) {
       return [...items].sort((a, b) => a.name.localeCompare(b.name));
     } else if (sortBy === "category") {
       return [...items].sort((a, b) => a.category.localeCompare(b.category));
-    } else if (sortBy === "grouped category") {
-      const grouped = items.reduce((acc, item) => {
-        if (!acc[item.category]) {
-          acc[item.category] = [];
-        }
-        acc[item.category].push(item);
-        return acc;
-      }, {});
-
-      const sortedGrouped = Object.keys(grouped)
-        .sort()
-        .map((category) => {
-          return {
-            category,
-            items: grouped[category].sort((a, b) =>
-              a.name.localeCompare(b.name)
-            ),
-          };
-        });
-
-      return sortedGrouped;
     }
     return items;
   };
@@ -65,21 +44,9 @@ export default function ItemList({ items }) {
         </button>
       </div>
       <ul>
-        {sortBy === "grouped category"
-          ? sortedItems().map((group) => (
-              <li
-                key={group.category}
-                className="capitalize text-black-600 text-center"
-              >
-                <h2>{group.category}</h2>
-                <ul>
-                  {group.items.map((item) => (
-                    <Item key={item.id} {...item} />
-                  ))}
-                </ul>
-              </li>
-            ))
-          : sortedItems().map((item) => <Item key={item.id} {...item} />)}
+        {sortedItems().map((item) => (
+          <Item key={item.id} {...item} />
+        ))}
       </ul>
     </div>
   );
--- app/week-7/item-list.js
+++ app/week-7/item-list.js
@@ -13,32 +13,41 @@ export default function ItemList({ items, onItemSelect }) {
     setSortBy("category");
   };
 
+  // const sortedItems = () => {
+  //   if (sortBy === "name") {
+  //     return [...items].sort((a, b) => a.name.localeCompare(b.name));
+  //   } else if (sortBy === "category") {
+  //     return [...items].sort((a, b) => a.category.localeCompare(b.category));
+  //   } else if (sortBy === "grouped category") {
+  //     const grouped = items.reduce((acc, item) => {
+  //       if (!acc[item.category]) {
+  //         acc[item.category] = [];
+  //       }
+  //       acc[item.category].push(item);
+  //       return acc;
+  //     }, {});
+
+  //     const sortedGrouped = Object.keys(grouped)
+  //       .sort()
+  //       .map((category) => {
+  //         return {
+  //           category,
+  //           items: grouped[category].sort((a, b) =>
+  //             a.name.localeCompare(b.name)
+  //           ),
+  //         };
+  //       });
+
+  //     return sortedGrouped;
+  //   }
+  //   return items;
+  // };
+
   const sortedItems = () => {
     if (sortBy === "name") {
       return [...items].sort((a, b) => a.name.localeCompare(b.name));
     } else if (sortBy === "category") {
       return [...items].sort((a, b) => a.category.localeCompare(b.category));
-    } else if (sortBy === "grouped category") {
-      const grouped = items.reduce((acc, item) => {
-        if (!acc[item.category]) {
-          acc[item.category] = [];
-        }
-        acc[item.category].push(item);
-        return acc;
-      }, {});
-
-      const sortedGrouped = Object.keys(grouped)
-        .sort()
-        .map((category) => {
-          return {
-            category,
-            items: grouped[category].sort((a, b) =>
-              a.name.localeCompare(b.name)
-            ),
-          };
-        });
-
-      return sortedGrouped;
     }
     return items;
   };
@@ -65,31 +74,9 @@ export default function ItemList({ items, onItemSelect }) {
         </button>
       </div>
       <ul>
-        {sortBy === "grouped category"
-          ? sortedItems().map((group) => (
-              <li
-                key={group.category}
-                className="capitalize text-black-600 text-center"
-              >
-                <h2>{group.category}</h2>
-                <ul>
-                  {group.items.map((item) => (
-                    <Item
-                      key={item.id}
-                      {...item}
-                      onselect={() => onItemSelect(item)}
-                    />
-                  ))}
-                </ul>
-              </li>
-            ))
-          : sortedItems().map((item) => (
-              <Item
-                key={item.id}
-                {...item}
-                onselect={() => onItemSelect(item)}
-              />
-            ))}
+        {sortedItems().map((item) => (
+          <Item key={item.id} {...item} onselect={() => onItemSelect(item)} />
+        ))}
       </ul>
     </div>
   );

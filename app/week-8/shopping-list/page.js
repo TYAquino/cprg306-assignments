83d6d54144f23d2e1596a174705a0ba1bc523451
Nabby <trishasalubreaquino@gmail.com>
2024-06-28 15:58:16 -0600

    commit

---
+++ app/week-8/shopping-list/page.js
@@ -0,0 +1,39 @@
+"use client";
+import React, { useState } from "react";
+import NewItem from "./new-item";
+import ItemList from "./item-list";
+import itemsData from "./items.json";
+import MealIdeas from "./meal-ideas";
+
+export default function Page() {
+  const [shoppingList, setItems] = useState(itemsData);
+  const [selectedItemName, setSelectedItemName] = useState("");
+
+  const handleAddItem = (newItem) => {
+    setItems([...shoppingList, newItem]);
+  };
+
+  const handleItemSelect = (item) => {
+    const cleanName = item.name
+      .split(",")[0]
+      .replace(
+        /([\u2700-\u27BF]|[\uE000-\uF8FF]|[\uD83C-\uDBFF][\uDC00-\uDFFF]|[\u2011-\u26FF]|[\uD83E-\uDD10-\uDDFF])/g,
+        ""
+      )
+      .trim();
+
+    setSelectedItemName(cleanName);
+  };
+
+  return (
+    <div className="flex">
+      <div className="w-1/2 p-4">
+        <NewItem onAddItem={handleAddItem} />
+        <ItemList items={shoppingList} onItemSelect={handleItemSelect} />
+      </div>
+      <div className="w-1/2 p-4">
+        <MealIdeas ingredient={selectedItemName} />
+      </div>
+    </div>
+  );
+}

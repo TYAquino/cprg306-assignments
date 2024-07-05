e45995e5af1be33f8596b640b982ddc7a04b4ed1
Nabby <trishasalubreaquino@gmail.com>
2024-07-04 23:16:43 -0600

    commit

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -17,33 +17,39 @@ export default function Page() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-200 flex justify-center items-center">
-      <div className="w-200 h-60 bg-white shadow-lg rounded-lg p-6 text-center">
-        <h1 className="text-3xl font-bold mb-10">Welcome to Shopping List</h1>
+    <div className="min-h-screen bg-gradient-to-r from-violet-600 to-blue-800 h-48 w-full flex justify-center items-center">
+      <div className="w-100 h-70 bg-white shadow-lg rounded-lg p-6 text-center">
+        <div className="text-3xl text-black font-bold mb-5">
+          <h1>Welcome to the Shopping List :3</h1>
+        </div>
+
         {!user ? (
-          <button
-            onClick={handleSignIn}
-            className="bg-blue-500 text-white px-4 py-2 rounded-md w-full mb-2"
-          >
-            Login with GitHub
-          </button>
+          <div>
+            <button
+              className="bg-sky-500 text-white px-4 py-2 rounded-md w-1/2 mb-2 hover:scale-110 transition duration-300 ease-in-out"
+              onClick={handleSignIn}
+            >
+              Login with GitHub
+            </button>
+          </div>
         ) : (
           <div className="mb-2">
-            <p className="text-gray-700 mb-2">
-              Welcome, {user.displayName} ({user.email})
-            </p>
-            <button
-              onClick={handleSignOut}
-              className="bg-red-500 text-white px-4 py-2 rounded-md w-full"
-            >
-              Logout
-            </button>
+            <p className="txt-black">Hello, {user.displayName}!</p>
+            <p className="txt-black">{user.email}</p>
+            <div>
+              <button
+                onClick={handleSignOut}
+                className="bg-red-500 mb-3 text-white px-4 py-2 rounded-md w-1/2"
+              >
+                Logout
+              </button>
+            </div>
           </div>
         )}
         <div>
           <Link
             href="/week-8/shopping-list"
-            className="text-blue-500 hover:underline"
+            className="text-green-500 hover:underline"
           >
             Go to Shopping List
           </Link>
--- app/week-8/shopping-list/page.js
+++ app/week-8/shopping-list/page.js
@@ -8,25 +8,22 @@ import itemsData from "./items.json";
 import MealIdeas from "./meal-ideas";
 
 export default function Page() {
-  const [shoppingList, setItems] = useState(itemsData);
+  const [items, setItems] = useState(itemsData);
   const [selectedItemName, setSelectedItemName] = useState("");
-  const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
-  const router = useRouter();
+  const { user } = useUserAuth();
+
+  const useCustomRouter = () => {
+    const router = useRouter();
+    return router;
+  };
+
+  const customRouter = useCustomRouter();
 
   useEffect(() => {
     if (!user) {
-      router.push("./week-8");
+      customRouter.push("./week-8");
     }
   }, [user, router]);
-
-  const handleSignIn = async () => {
-    await gitHubSignIn();
-    router.push("./week-8/shopping-list");
-  };
-
-  const handleSignOut = async () => {
-    await firebaseSignOut();
-  };
 
   const handleAddItem = (newItem) => {
     setItems([...shoppingList, newItem]);
@@ -45,33 +42,13 @@ export default function Page() {
   };
 
   if (!user) {
-    return (
-      <div className="min-h-screen bg-gray-200 flex justify-center items-center">
-        <div className="w-200 h-60 bg-white shadow-lg rounded-lg p-6 text-center">
-          <h1 className="text-3xl font-bold mb-10">Welcome to Shopping List</h1>
-          <button
-            onClick={handleSignIn}
-            className="bg-blue-500 text-white px-4 py-2 rounded-md w-full mb-2"
-          >
-            Login with GitHub
-          </button>
-          <div>
-            <Link
-              href="/week-8/shopping-list"
-              className="text-blue-500 hover:underline"
-            >
-              Go to Shopping List
-            </Link>
-          </div>
-        </div>
-      </div>
-    );
+    return null;
   }
 
   return (
     <main className="p-8 bg-teal-500 min-h-screen">
       <h1 className="text-xl font-bold mb-4">Shopping List</h1>
-      <div style={{ display: "flex", justifyContent: "pace-between" }}>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
         <div>
           <NewItem onAddItem={handleAddItem} />
           <ItemList items={items} onItemSelect={handleItemSelect} />
@@ -79,17 +56,6 @@ export default function Page() {
         <div className="flex flex-col items-left w-1/4 p-4">
           {selectedItemName && <MealIdeas ingredient={selectedItemName} />}
         </div>
-      </div>
-      <div className="mb-2">
-        <p className="text-gray-700 mb-2">
-          Welcome, {user.displayName} ({user.email})
-        </p>
-        <button
-          onClick={handleSignOut}
-          className="bg-red-500 text-white px-4 py-2 rounded-md w-full"
-        >
-          Logout
-        </button>
       </div>
     </main>
   );
